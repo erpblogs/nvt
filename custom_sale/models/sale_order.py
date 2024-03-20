1dbdd0ec590b6297c62a68b84fb3c2749606ac84
TrinhQuangMinhVTI <131467348+TrinhQuangMinhVTI@users.noreply.github.com>
2024-03-21 06:38:30 +0700

    update

--- custom_sale/models/sale_order.py
+++ custom_sale/models/sale_order.py
@@ -23,6 +23,20 @@ class SaleOrderInherit(models.Model):
             readonly=False, store=True, 
             inverse="_inverse_approval_state",
             default='draft')
+    old_state = fields.Selection(
+            selection=APPROVAL_STATE,
+            string="Old Status",
+            compute='compute_approval_state',
+            readonly=False, store=True,
+            default='draft')
+
+    def compute_approval_state(self):
+        for r in self:
+            if r.approval_state:
+                r.old_state = r.approval_state
+            else:
+                r.old_state = False
+
     
     @api.onchange('approval_state')
     def _inverse_approval_state(self):
@@ -35,5 +49,18 @@ class SaleOrderInherit(models.Model):
                 r.state = 'draft'
             else:
                 r.state = 'sale'
-                
+            template = self.env.ref('custom_sale.mail_template_change_stage_sale')
+            values = {
+
+            }
+            template.send_mail(r.id)
+            r.old_state = r.approval_state
+
+    def get_old_state(self):
+         if self.old_state:
+             return dict(self._fields['old_state']._description_selection(self.env)).get(self.old_state)
+
+    def get_new_state(self):
+         if self.old_state:
+             return dict(self._fields['old_state']._description_selection(self.env)).get(self.approval_state)
     
