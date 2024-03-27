3b2f61546de6e9e10accbc4684b37a07e570eb5f
quangtv <trinhvanquangf1@gmail.com>
2024-03-27 02:15:48 +0000

    custom layout

--- custom_website/controllers/main.py
+++ custom_website/controllers/main.py
@@ -88,7 +88,10 @@ class WebsiteSaleCustom(WebsiteSale):
         attributes_ids = {v[0] for v in attrib_values}
         attrib_set = {v[1] for v in attrib_values}
 
-        filter_by_tags_enabled = website.is_view_active('website_sale.filter_products_tags')
+        # filter_by_tags_enabled = website.is_view_active('website_sale.filter_products_tags')
+        # alway active 
+        filter_by_tags_enabled = True
+        
         if filter_by_tags_enabled:
             tags = request_args.getlist('tags')
             # Allow only numeric tag values to avoid internal error.
@@ -254,11 +257,13 @@ class WebsiteSaleCustom(WebsiteSale):
             values['max_price'] = max_price or available_max_price
             values['available_min_price'] = tools.float_round(available_min_price, 2)
             values['available_max_price'] = tools.float_round(available_max_price, 2)
-        # if filter_by_tags_enabled:
-        #     values.update({'all_tags': all_tags, 'tags': tags})
+        if filter_by_tags_enabled:
+            values.update({'all_tags': all_tags, 'tags': tags})
         # alway allow tags filter
-        values.update({'all_tags': all_tags, 'tags': tags})
+        # values.update({'all_tags': all_tags, 'tags': tags})
         if category:
             values['main_object'] = category
         values.update(self._get_additional_shop_values(values))
+        
+        # return request.render("custom_website.products_base", values)
         return request.render("custom_website.products", values)
